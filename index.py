518aaa2ca71b4674291c7220233e99d77e000a4c
Bha12vya <bhavyabarapati12@gmail.com>
2025-02-19 10:46:52 +0530

    commit

--- index.py
+++ index.py
@@ -590,3 +590,53 @@
 #             temp = temp // 10
 #         if i == sum:
 #             print(i)
+
+#------------------------------------------------------------------
+#Wap to check if the digits of each number in an list are in increasing order, returning true or false for each Increasing order or not
+
+
+# num1 = [253, 678, 258, 321]
+# for number in num1:
+#     str_number = str(number)
+#     increasing = True
+#     for i in range(len(str_number) - 1):
+#         if str_number[i] >= str_number[i + 1]:  
+#             increasing = False
+#             break
+    
+    
+#     print(increasing)
+
+    
+ #----------------------------------------------------------------------   
+    
+#Wap to check if the digits of each number in an list are in decreasing order and return an array of true otherwise false.Decreasing order -true   
+
+# num2 = [111, 321, 300, 652, 789]
+# result = []  
+# for number in num2:
+#     str_number = str(number)
+#     decreasing = True  
+#     for i in range(len(str_number) - 1):
+#         if str_number[i] <= str_number[i + 1]:   
+#            decreasing = False
+#            break  
+#     result.append(decreasing)
+# print(result)
+
+
+#------------------------------------------------------------------
+
+#  Finding the frequency of elements in an array.
+
+# arr1 = [10, 30, 10, 20, 10, 20, 30, 10]
+# frequency = {}
+# for num in arr1:
+#     if num in frequency:
+#         frequency[num] += 1   
+#     else:
+#         frequency[num] = 1  
+# for key, value in frequency.items():
+#     print(f"{key} => {value}")
+
+#-------------------------------------------------------------------
